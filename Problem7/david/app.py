389d1458afaba831532e49f40490b3d50d7ed28b
juheon <85102107+namgoygoy@users.noreply.github.com>
2025-08-08 14:07:00 +0900

    Update app.py

--- Problem7/david/app.py
+++ Problem7/david/app.py
@@ -46,5 +46,9 @@ def index():
 def menu():
     return render_template('menu.html')
 
+@app.route("/test2")
+def test2():
+    return render_template('test2.html')
+
 if __name__ == "__main__":
     app.run("0.0.0.0", 80, debug=True)
