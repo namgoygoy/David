891dbe0e9ac0ad22c49c9e016efa993162d724d4
juheon <juheon5145@gmail.com>
2025-08-08 14:05:13 +0900

    라우트 추가

--- Problem7/david/app.py
+++ Problem7/david/app.py
@@ -46,5 +46,9 @@ def index():
 def menu():
     return render_template('menu.html')
 
+@app.route("/test1")
+def test1():
+    return render_template('test1.html')
+
 if __name__ == "__main__":
     app.run("0.0.0.0", 80, debug=True)
