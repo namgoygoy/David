0d75013269502c8768f0297a9543a4cb4d725882
juheon <juheon5145@gmail.com>
2025-08-08 14:28:39 +0900

    test 삭제

--- Problem7/david/app.py
+++ Problem7/david/app.py
@@ -46,13 +46,5 @@ def index():
 def menu():
     return render_template('menu.html')
 
-@app.route("/test1")
-def test1():
-    return render_template('test1.html')
-
-@app.route("/test2")
-def test2():
-    return render_template('test2.html')
-
 if __name__ == "__main__":
     app.run("0.0.0.0", 80, debug=True)
